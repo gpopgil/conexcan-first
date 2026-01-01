91da0521b94fcbf60f07dc20be9aa41e98402ee6
gheorghe <gpopgil@gmail.com>
2026-01-01 10:55:14 +0000

    Cleaning vite/react template

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,12 +9,14 @@ export default function Header(){
         <div className="menu-wrapper">
 
             <div className="logo-wrapper">
-
+      <a href="https://vite.dev" target="_blank">
+          <img src={conexcanLogo} className="logo" alt="Vite logo" />
+        </a>
             </div>
             <div className="menu-nav">
 
             </div>
-            
+
         </div>
         </>
 
